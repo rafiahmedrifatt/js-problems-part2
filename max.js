0a364832a9311231fcd8cdbf067a6a6bd601c9b4
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-02-15 15:25:43 +0600

    added max and max 3

---
+++ max.js
@@ -0,0 +1,18 @@
+const disha = 56;
+const salmon = 62;
+
+if (disha > salmon) {
+  console.log("disha will get the strawberry");
+} else {
+  console.log("disha will not get the strawberry");
+}
+
+function getMax(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  return num2;
+}
+
+const max1 = getMax(34, 89);
+console.log(max1);
